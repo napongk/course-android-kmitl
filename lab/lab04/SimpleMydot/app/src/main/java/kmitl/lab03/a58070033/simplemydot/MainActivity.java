79e78d0676c45eff944e4bbb9bda2f62288fe843
napongk <58070033@kmitl.ac.th>
2017-09-13 20:44:17 +0700

    Update Share Buttton

--- lab/lab04/SimpleMydot/app/src/main/java/kmitl/lab03/a58070033/simplemydot/MainActivity.java
+++ lab/lab04/SimpleMydot/app/src/main/java/kmitl/lab03/a58070033/simplemydot/MainActivity.java
@@ -2,21 +2,29 @@ package kmitl.lab03.a58070033.simplemydot;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.Random;
 
 import kmitl.lab03.a58070033.simplemydot.model.Colors;
@@ -32,9 +40,9 @@ public class MainActivity extends AppCompatActivity
     private DotView dotView;
     private Dots dots;
     final private int REQUEST_CODE_EXTERNAL_STORAGE = 1;
-    String[] choose = {"Change Color", "Change Size", "Delete"};
 
-    ScreenCapture capt;
+    ScreenCapture capt = new ScreenCapture();
+
 
 
     @Override
@@ -42,7 +50,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
 
 
         dotView = (DotView) findViewById(R.id.dotView);
@@ -51,16 +58,6 @@ public class MainActivity extends AppCompatActivity
         dots = new Dots();
         dots.setListener(this);
 
-        Button shareButton = (Button) findViewById(R.id.sharebutton);
-        shareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                askforPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,REQUEST_CODE_EXTERNAL_STORAGE);
-                Bitmap bm = ScreenCapture.takescreenshotOfRootView(rootView);
-                Uri uri = ScreenCapture.saveBitmap(bm);
-                useShare(uri);
-            }
-        });
     }
 
     public void onRandomDot(View view) {
@@ -93,11 +90,23 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private void askforPermission(String permission, int requestCode){
+    private boolean askforPermission(String permission, int requestCode){
         if (ContextCompat.checkSelfPermission(this,permission)!=PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this,new String[]{permission}, requestCode);
+            Toast.makeText(this.getApplicationContext(), "Permission Granted please press button again", Toast.LENGTH_SHORT);
+            return false;
         }
+        else {
+            return true;
+        }
+    }
 
+    public void onShare (View view){
+        if(askforPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,REQUEST_CODE_EXTERNAL_STORAGE)) {
+            Bitmap bm = ScreenCapture.takescreenshotOfRootView(getWindow().getDecorView().findViewById(android.R.id.content));
+            Uri uri = getImageUri(this.getApplicationContext(), bm);
+            useShare(uri);
+        }
     }
 
     public void useShare(Uri uri){
@@ -122,4 +131,13 @@ public class MainActivity extends AppCompatActivity
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
+
+    public Uri getImageUri(Context context, Bitmap bitmap){
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String imgPath = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "DotBitmap", null);
+        return Uri.parse(imgPath);
+    }
+
+
 }
