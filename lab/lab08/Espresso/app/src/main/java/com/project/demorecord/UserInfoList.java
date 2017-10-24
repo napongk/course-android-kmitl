fa80cddccabb1a8cf3e84a88db2d9f139296b0f7
Unknown <58070033@kmitl.ac.th>
2017-10-25 02:05:20 +0700

    Lab 08 Homework : add Test case & add Clear List

---
+++ lab/lab08/Espresso/app/src/main/java/com/project/demorecord/UserInfoList.java
@@ -0,0 +1,19 @@
+package com.project.demorecord;
+
+import java.util.List;
+
+public class UserInfoList {
+    public List<UserInfo> getUserInfoList() {
+        return userInfoList;
+    }
+
+    public void setUserInfoList(List<UserInfo> userInfoList) {
+        this.userInfoList = userInfoList;
+    }
+
+    public void clearUserInfoList(List<UserInfo> userInfoList){
+        userInfoList.clear();
+    }
+
+    private List<UserInfo> userInfoList;
+}
